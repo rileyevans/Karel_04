635f21c866c045f7913a7f5ee26634047810b3ad
rileyevans <riley_evans@asl.org>
2014-11-11 15:04:18 +0000

    code

--- Lab04.java
+++ Lab04.java
@@ -2,8 +2,8 @@
 *
 * Description of the program goes here  // provide a brief description
 *
-* @author <Your Name here>  // replace <...> with your name
-* @version <date/of/completion> // replace <...> with the date
+* @author <Riley Evans>  // replace <...> with your name
+* @version <11/3/2014> // replace <...> with the date
 */
 
 import edu.fcps.karel2.Display;
@@ -11,7 +11,22 @@ import edu.fcps.karel2.Robot;
 
 public class Lab04 {
 
-     public static void main(String[] args) {
-          // TODO Your code goes here
+  public static void main(String[] args)  
+  {      
+   Display.openWorld("maps/arena.map");
+   Display.setSize(10, 10);
+   Display.setSpeed(5);
+
+      Athlete maria = new Athlete();
+      Athlete riley = new Athlete();
+      Athlete manu = new Athlete();
+      Athlete chloe = new Athlete();
+      Athlete andre = new Athlete();
+      Athlete sam = new Athlete();
+      
+riley.turnRight(); 
+
+
+  
      }
 }
---
+++ takeTheField.java
@@ -0,0 +1,10 @@
+  public static void takeTheField(Athlete arg)  
+  {	
+      arg.move();	
+      arg.move();	
+      arg.move();	
+      arg.move();	
+      arg.turnRight();	
+      arg.move();	
+      arg.move();  
+  }  
